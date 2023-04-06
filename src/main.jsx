d4e59e1bd5694a6c1730e0ec6f513d94d3c194a6
sohanur28 <gsrahman28@gmail.com>
2023-04-06 21:05:15 +0600

    activeLink-loading-sppiner

--- src/main.jsx
+++ src/main.jsx
@@ -67,6 +67,10 @@ const router =createBrowserRouter([
       {
         path: 'contact',
         element: <Contact></Contact>
+      },
+      {
+        path: '*',
+        element: <div>404 Not founded</div>
       }
     ]
   }
